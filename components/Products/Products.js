f9f3c40224fe66c0279f22e8ab1db77ed9a47eba
zero <artemlysakov793@gmail.com>
2024-07-03 19:33:54 +0300

    add onclick
    Реализация события клик при котором данные по карточке записываются в Store

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,17 +1,46 @@
 class Products {
+    constructor(){
+        this.classNameActive = `products-element__button_activ`;
+        this.labelAdd = `Добавить в корзину`;
+        this.labelRemov = `Удалить из корзины`;
+    }
 
+    hendlSetLocationStorage(element, id){
+        const { pushProduct, products } = localStorageUtil.putProducts(id)
+    
+        if(pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemov;
+        } else {            
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+    }
 
     render(){
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
         CATALOG.forEach(({id, name, price, img})=>{
+            let activClass = '';
+            let activText ='';
+            if (productsStore.indexOf(id) === -1){
+                activText = this.labelAdd
+            } else {
+                activClass = " " + this.classNameActive 
+                activText = this.labelRemov 
+            }
 
             htmlCatalog += `
             <li class="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src="${img}"/>
                 <span class="products-element__price">😻 ${price.toLocaleString()} V</span>
-                <button class="products-element__button"> Добавить в корзину </button>
+                <button 
+                onclick="productsPage.hendlSetLocationStorage(this, '${id}')" 
+                class="products-element__button ${activClass}"> 
+                ${activText}
+                </button>
             </li>
             `;
 
