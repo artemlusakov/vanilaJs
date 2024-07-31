fbbffc66014e140cf9689b2f7460d38eff36ea97
zero <artemlysakov793@gmail.com>
2024-07-31 10:36:23 +0300

    test

--- Toodu/app.js
+++ Toodu/app.js
@@ -2,38 +2,80 @@ const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-const notes = ['Записать что-то важное', 'Решить продлемы', 'lol']
+// const notes = ['Записать что-то важное', 'Решить продлемы', 'lol']
 
-function render() {
+// function render() {
     // for (let i = 0 ; i < notes.length; i++) {
     //     listElement.insertAdjacentHTML('beforeend',GetNoteTemplate(notes[i]))
 
     // }
 
+//     for (let note of notes) {
+//         listElement.insertAdjacentHTML('beforeend',GetNoteTemplate(note))
+//
+//     }
+// }
+
+// render()
+
+// createBtn.onclick = function(){
+//     if(inputElement.value.lenght === 0){
+//         console.log(1)
+//         return
+//     }
+//     listElement.insertAdjacentHTML('beforeend', GetNoteTemplate(inputElement.value))
+//     inputElement.value = ''
+// }
+
+// function GetNoteTemplate(title) {
+//     return`
+//     <li
+//     class="list-group-item d-flex justify-content-between align-items-center"
+//     >
+//         <span>${title}</span>
+//     <span>
+//     <span class="btn btn-small btn-success">&check;</span>
+//     <span class="btn btn-small btn-danger">&times;</span>
+//     </span>
+//
+// </li>
+// `
+// }
+const notes = [
+    {
+        titel: 'Записать что-то важное',
+        compliter: false,
+    },
+    {
+        titel: 'Записать что-то',
+        compliter: true,
+    },
+    {
+        titel: 'Записать важное',
+        compliter: false,
+    },
+]
+
+function render () {
     for (let note of notes) {
         listElement.insertAdjacentHTML('beforeend',GetNoteTemplate(note))
-
-    }
+        console.log(note)
 }
-
-render()
+}
 
 createBtn.onclick = function(){
     if(inputElement.value.lenght === 0){
-        console.log(1)
-        return 
+        return
     }
     listElement.insertAdjacentHTML('beforeend', GetNoteTemplate(inputElement.value))
-
+    console.log(12)
     inputElement.value = ''
 }
 
-function GetNoteTemplate(title) {
-    return        `
-    <li
-    class="list-group-item d-flex justify-content-between align-items-center"
-    >
-        <span>${title}</span>
+function GetNoteTemplate(note) {
+    return `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${note.titel}</span>
     <span>
     <span class="btn btn-small btn-success">&check;</span>
     <span class="btn btn-small btn-danger">&times;</span>
@@ -43,5 +85,4 @@ function GetNoteTemplate(title) {
 `
 }
 
-const person = {}
-console.log(typeof person)
+render()
